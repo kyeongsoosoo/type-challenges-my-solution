f80886cb2f4e593f8de1318789a06a10e1652df4
kyeongsoosoo <shakramon@naver.com>
2021-06-29 15:39:34 +0900

    easy-first-of-array

---
+++ easy/first-of-array.ts
@@ -0,0 +1,10 @@
+
+type First<T extends any[]> = T extends [] ? never : T[0];
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+type arr3 = [];
+
+type head1 = First<arr1> // expected to be 'a'
+type head2 = First<arr2> // expected to be 3
+type head3 = First<arr3> // expected to be 3
--- easy/tuple-to-object.ts
+++ easy/tuple-to-object.ts
@@ -5,5 +5,5 @@
         [P in T[number]] : P;
     }
     const result: TupleToObject<typeof tuple> // expected 
-    ={ tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+    ={ "tesla": 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
 }
