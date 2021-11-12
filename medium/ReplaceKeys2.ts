e828759c6691c083c62aa4a8e16fd396464a1866
kyeongsoosoo <shakramon@naver.com>
2021-11-12 13:56:15 +0900

    11/12

---
+++ medium/ReplaceKeys2.ts
@@ -0,0 +1,42 @@
+{
+    type Map<O> = {
+        [Keys in keyof O]: O[Keys]
+    }
+
+    type ReplaceKeys<U, R extends string, O = { [keys in R]: any}> = 
+        U extends U
+            ? {
+                [Keys in keyof U]: Keys extends R 
+                                    ? Keys extends keyof O 
+                                        ? O[Keys] 
+                                        : never
+                                    : U[Keys]
+            }
+            : never
+
+            type NodeA = {
+                type: 'A'
+                name: string
+                flag: number
+              }
+              
+              type NodeB = {
+                type: 'B'
+                id: number
+                flag: number
+              }
+              
+              type NodeC = {
+                type: 'C'
+                name: string
+                flag: number
+              }
+              
+              
+              type Nodes = NodeA | NodeB | NodeC
+              
+              type ReplacedNodes = ReplaceKeys<Nodes, 'name' | 'flag', {name: number, flag: string}> // {type: 'A', name: number, flag: string} | {type: 'B', id: number, flag: string} | {type: 'C', name: number, flag: string} // would replace name from string to number, replace flag from number to string.
+              
+              type ReplacedNotExistKeys = ReplaceKeys<Nodes, 'name', {aa: number}> // {type: 'A', name: never, flag: number} | NodeB | {type: 'C', name: never, flag: number} // would replace name to never
+              
+}
